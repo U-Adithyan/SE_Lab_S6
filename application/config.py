471fcbe1d0ab5b0405a257559927980e8abdf4b1
U Adithyan <82749073+U-Adithyan@users.noreply.github.com>
2023-01-15 18:10:14 +0530

    Add files via upload

--- application/config.py
+++ application/config.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-class Config():
+class Config:
   DEBUG=True
   DATABASE = os.path.join(BASE_DIR, "../database.db")
   SECRET_KEY = 'hjdfhhjdijmcvkjesikhj'
